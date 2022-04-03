cdcb5b747f68e107cf0919af31dd960b4087229d
arsalankhan-17 <arsalankhan4014@gmail.com>
2022-04-03 15:21:08 +0500

    added extra views and controller methods

--- models/product.js
+++ models/product.js
@@ -1,16 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
 const products = [];
+const filePath = path.join(rootDir,'data','products.json');
+const getProductsFromFile = cb =>{
+   
+    fs.readFile(filePath,(err,fileContent)=>{
+        if(err){
+           cb([]);
+        }else{
+
+            cb(JSON.parse(fileContent));
+        }
+
+       
+    });
+}
 
 module.exports = class Product{
 
-    constructor(t){
-        this.title = t;
+    constructor(title,imageUrl,price,description){
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.description = description;
     }
 
     save(){
+
+       getProductsFromFile(products=>{
         products.push(this);
+        fs.writeFile(filePath,JSON.stringify(products),(err)=>{
+        console.log(err);
+        });
+   
+       });
+           
+       
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+       
+        getProductsFromFile(cb);      
     }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const AdminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', AdminController.getAddProduct);
+router.get('/products', AdminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', AdminController.postAddProduct);
 
 module.exports = router;
