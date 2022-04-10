ec7f0cfeedaca1cb20d65f1dd53b4d34075af242
arsalankhan-17 <arsalankhan4014@gmail.com>
2022-04-10 19:43:38 +0500

    added edit product functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,4 @@
+const Cart = require('../models/cart');
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
@@ -38,6 +39,16 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+
+  const prodId = req.body.productId;
+  Product.findProductById(prodId,product=>{
+
+    Cart.addProduct(prodId,product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
