0f6fc6b37192e77276b6105ce4835e492d4954b4
arsalankhan-17 <arsalankhan4014@gmail.com>
2022-04-12 23:36:13 +0500

    added mysql

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./util/database');
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -12,6 +13,11 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('select * from products').then(result=>console.log(result)).catch(err=>console.log(err));
+
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
