7f700efcb60b1a7e97c067afa09d87344487fafb
arsalankhan-17 <arsalankhan4014@gmail.com>
2022-04-19 21:51:07 +0500

    delete from cart functionality

--- models/product.js
+++ models/product.js
@@ -1,33 +1,27 @@
-const Sequelize= require('sequelize');
+const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-
-const Product = sequelize.define('product',{
-
-  id:{
-    type:Sequelize.INTEGER,
-    allowNull:false,
-    autoIncrement:true,
-    primaryKey:true
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
   },
-  title:{
-    type:Sequelize.STRING,
-    allowNull:false,
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
   },
-  price:{
-    type:Sequelize.DOUBLE,
-    allowNull:false,
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
-  imageUrl:{
-    type:Sequelize.STRING,
-    allowNull:false,
-  },
-  description:{
-    type:Sequelize.STRING,
-    allowNull:false,
-  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
-
 
 module.exports = Product;
