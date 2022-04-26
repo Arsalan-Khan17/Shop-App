eb351eed5980de5c548134371d7dc03240552f89
arsalankhan-17 <arsalankhan4014@gmail.com>
2022-04-27 00:25:12 +0500

    edit and delete with mongodb

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const mongoConnect = require('./util/database');
 const errorController = require('./controllers/error');
 const app = express();
 
+const mongoConnect = require('./util/database').mongoConnect;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+ const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,14 +23,14 @@ app.use((req, res, next) => {
 //       next();
 //     })
 //     .catch(err => console.log(err));
+next();
 });
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+ app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client=>{
-    console.log(client);
+mongoConnect(()=>{
     app.listen(3000);
 });
--- util/database.js
+++ util/database.js
@@ -1,15 +1,24 @@
 const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
-
+let _db;
 const mongoConnect = callback=>{
-    MongoClient.connect('mongodb+srv://AK:2GzOsqhQGbET3eOQ@ak-cluster.aicwf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://AK:Dm8L513KYCg7y8Ij@ak-cluster.aicwf.mongodb.net/shop_app?retryWrites=true&w=majority')
     .then(client=>{
         console.log('connected');
-        callback(client);
+        _db = client.db();
+        callback();
     }).catch(err=>{
         console.log(err);
     });
 };
 
-module.exports = mongoConnect;
+const getDb = ()=>{
+if(_db){
+    return _db;
+}
+throw 'No Database Found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
